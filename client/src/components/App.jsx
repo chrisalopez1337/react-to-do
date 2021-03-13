5b639455a7085e94592a5090b41d9291ea4c339e
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-12 20:33:12 -0800

    Added base styling and handler for the creation bar

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,8 @@
 import React, { useState, useEffect } from 'react';
 import { setOne, getOne, deleteOne, getAll } from 'local-js';
 import styled from 'styled-components';
+// Children components
+import CreateItem from './CreateItem.jsx';
 
 // Styling
 const Container = styled.div`
@@ -12,9 +14,13 @@ const Container = styled.div`
 `;
 
 export default function App() {
+    // Hold all to-do items
+    const [toDoItems, setToDoItems] = useState([]);
+
     return (
         <Container>
             <h1>React-To-Do</h1>
+            <CreateItem />
         </Container>
     );
 };
---
+++ client/src/components/CreateItem.jsx
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from 'react';
+import { getOne, setOne } from 'local-js';
+import styled from 'styled-components';
+
+// Styling
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+`;
+
+const Input = styled.input`
+    border: 2px solid #9326c9;
+    border-radius: 7px;
+    padding: 7px;
+    font-size: 16px;
+    color: whitesmoke;
+    font-family: inherit;
+    background-color: transparent;
+    margin-left: 10px;
+`;
+
+const Button = styled.button`
+    border: 2px solid #9326c9;
+    border-radius: 7px;
+    padding: 7px;
+    color: whitesmoke;
+    font-family: inherit;
+    background-color: transparent;
+    margin-left: 10px;
+    font-size: 16px;
+    font-weight: bold;
+    transition-duration: 0.2s;
+    &:hover {
+        background-color: #9326c9;
+        border: 2px solid whitesmoke;
+    }
+`;
+
+export default function CreateItem() {
+    // Hold Item title
+    const [title, setTitle] = useState('');
+    // Title handler
+    function handleTitle(e) {
+        setTitle(e.target.value);
+    }
+    return (
+        <Container>
+            <h3>New To-Do:</h3>
+            <Input placeholder="To-Do Name..." value={title} onChange={handleTitle}/>
+            <Button>Add</Button>
+        </Container>
+    );
+}
