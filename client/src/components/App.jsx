d85b2ef28c98edf1428d08504a07c45bcffd10d0
chrisalopez1337 <chrisalopez@outlook.com>
2021-03-12 19:54:12 -0800

    Added base structure for App component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+// Dependencies
+import React, { useState, useEffect } from 'react';
+import { setOne, getOne, deleteOne, getAll } from 'local-js';
+import styled from 'styled-components';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = 
-            {
+// Styling
+const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`;
 
-            }
-    }
-
-    render() {
-        return (
-
-        );
-    }
-}
-
-export default App;
+export default function App() {
+    return (
+        <Container>
+            <h1>React-To-Do</h1>
+        </Container>
+    );
+};
